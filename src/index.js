45f6fd37258f336dae652255570303150c53ac9c
wira2106 <wiranataputra21@gmail.com>
2024-10-01 05:14:35 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let nama = 'Putra wiranata';
+let nim = 160030071;
+let bebas = "penggnaan";
+let bebasTerprediksi;
+let arrayString;
+bebas += 123;
+bebasTerprediksi = 123;
+arrayString = ["testing", "testing1"];
+console.log({ nama });
+console.log({ nim });
+console.log({ bebas });
+console.log({ arrayString });
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let nama:String = 'Putra wiranata';
+let nim:Number = 160030071;
+let bebas:any = "penggnaan";
+let bebasTerprediksi: number|string|boolean;
+let arrayString:string[];
+let arrayString2:[string,string];
+
+bebas += 123;
+bebasTerprediksi = 123
+arrayString = ["testing","testing1"];
+
+console.log({nama});
+console.log({nim});
+console.log({bebas});
+console.log({arrayString});
